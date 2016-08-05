2752f874fd2827c58beeffcf76a24c0fe5004f79
Reedy <reedy@wikimedia.org>
2016-08-05 22:00:02 +0100

    De-capitalise timeline in wfLoadExtension call
    Change-Id: I2c1501e8bec4d5dc04a0a125603d867a6438176e

--- Timeline.php
+++ Timeline.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'Timeline' );
+	wfLoadExtension( 'timeline' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Timeline'] = __DIR__ . '/i18n';
 /*	wfWarn(
