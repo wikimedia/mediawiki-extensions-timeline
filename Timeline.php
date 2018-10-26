2dff8476aebe47d7b18acc40688a82b3d3f7050f
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 22:01:07 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: Ie420be825158306d66741e8f1862ddd0a976b7f8

--- Timeline.php
+++ Timeline.php
@@ -4,13 +4,13 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'timeline' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Timeline'] = __DIR__ . '/i18n';
-/*	wfWarn(
-		'Deprecated PHP entry point used for Timeline extension. ' .
+	wfWarn(
+		'Deprecated PHP entry point used for timeline extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
-*/
+
 	return;
 } else {
-	die( 'This version of the Timeline extension requires MediaWiki 1.29+' );
+	die( 'This version of the timeline extension requires MediaWiki 1.29+' );
 }
